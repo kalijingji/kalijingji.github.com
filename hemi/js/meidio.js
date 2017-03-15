2805915ea934ed8f0d2feaac22603084854811fa
kalijingji <yinyonggui1992@qq.com>
2017-03-15 16:58:06 +0800

    oi
    hh

---
+++ hemi/js/meidio.js
@@ -0,0 +1,22 @@
+function changerem(){
+				setTimeout(function(){
+				var width=$(window).width();
+				if(width>640){
+					width=640;
+				}
+				if(width<320){
+					width=320;
+				}
+				$("html").css("font-size",width/16)
+				},100)
+		}
+$(window).ready(function(){	
+				changerem()
+			})
+			changerem()
+$(window).resize(function(){
+				changerem()
+			})
+setTimeout(function(){
+	
+})
